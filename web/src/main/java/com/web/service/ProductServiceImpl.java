a53f720b4095719e1bc8ca4fa8dbc2cc6dab2b41
ArjunMaadhava <ArjunMaadhava@pc>
2017-08-12 17:14:56 +0530

    changes1

--- web/src/main/java/com/web/service/ProductServiceImpl.java
+++ web/src/main/java/com/web/service/ProductServiceImpl.java
@@ -1,8 +1,17 @@
 package com.web.service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.web.model.CarouselItem;
 import com.web.model.Category;
 import com.web.model.ImageDetail;
@@ -66,20 +75,41 @@ public class ProductServiceImpl implements ProductService {
 	@Override
 	public List<Product> getBestSellers() {
 		List<Product> bestSellingItems= new ArrayList<>();
-		for(int i=0;i<10;i++){
-			Product pr= new Product();
-			pr.setProductName("Product Name"+i);
-			ImageDetail id= new ImageDetail();
-			id.setImgLocation(""+i+".jpg");
-			pr.setDefaultImage(id);
-			pr.setShortDesc("Short Description"+i);
-			//pr.setMrp(100);
-			//pr.setMeasuringUnit(KG);
-			//pr.setScale(1);
-			bestSellingItems.add(pr);
-			System.out.println(pr.getDefaultImage().getImgLocation());
-		}
+		//get/AllProducts
+		Gson gson = new Gson();
+		String result = callRESTservice("get/AllProducts");
+		bestSellingItems = gson.fromJson(result, new TypeToken<List<Product>>(){}.getType());
 		return bestSellingItems;
 	}
+	private String callRESTservice(String serviceName) {
+		try {
+			CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+			String restURL = "http://localhost:8089/" + serviceName;
+			// StringEntity input = new StringEntity(inputString);
+			System.out.println(restURL);
+			HttpGet getRequest = new HttpGet(restURL);
+//			HttpPost postRequest = new HttpPost(restURL);
+//			postRequest.addHeader("content-type", "application/json");
+//			postRequest.addHeader("Accept", "application/json");
+			// postRequest.setEntity(input);
+			HttpResponse response = httpClient.execute(getRequest);
+
+			if (response.getStatusLine().getStatusCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : " + response.getStatusLine().getStatusCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+			String output;
+			StringBuilder result = new StringBuilder();
+			while ((output = br.readLine()) != null) {
+				result.append(output);
+			}
+			return result.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
 
 }
