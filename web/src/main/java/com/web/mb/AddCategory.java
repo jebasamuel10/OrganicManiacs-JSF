133e23cf27ec6b788fba66a9227fdae05fbedc40
ArjunMaadhava <ArjunMaadhava@pc>
2017-08-12 16:59:43 +0530

    changes

--- web/src/main/java/com/web/mb/AddCategory.java
+++ web/src/main/java/com/web/mb/AddCategory.java
@@ -23,40 +23,48 @@ public class AddCategory implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private Category category;
 	private Product selectedProduct;
-	
+
 	public Product getSelectedProduct() {
 		return selectedProduct;
 	}
+
 	public void setSelectedProduct(Product selectedProduct) {
 		this.selectedProduct = selectedProduct;
 	}
+
 	public Category getCategory() {
 		return category;
 	}
+
 	public void setCategory(Category category) {
 		this.category = category;
 	}
+
 	@PostConstruct
-	public void init(){
-		category=new Category();
+	public void init() {
+		category = new Category();
 	}
-	public void saveProduct(){
+
+	public void saveProduct() {
 		AdminServiceImpl.getObject().addCategory(category);
 	}
-	public void delProduct(){
+
+	public void delProduct() {
 		category.getProductList().remove(selectedProduct);
 	}
-	 public void handleFileUpload(FileUploadEvent event) {
-		 UploadedFile uploadedFile = event.getFile();
-		    String fileName = uploadedFile.getFileName();
-		    String contentType = uploadedFile.getContentType();
-		    byte[] contents = uploadedFile.getContents(); // Or getInputStream()
-		    ImageDetail id = new ImageDetail();
-		    id.setImgage(uploadedFile);
-		    id.setAltText(fileName);
-		    category.setImg(id);
-	 }
-	 public void saveCategory(){
-		 AdminServiceImpl.getObject().addCategory(category);
-	 }
+
+	public void handleFileUpload(FileUploadEvent event) {
+		UploadedFile uploadedFile = event.getFile();
+		String fileName = uploadedFile.getFileName();
+		String contentType = uploadedFile.getContentType();
+		byte[] contents = uploadedFile.getContents(); // Or getInputStream()
+		ImageDetail id = new ImageDetail();
+		id.setImgage(uploadedFile);
+		id.setAltText(fileName);
+		category.setImg(id);
+	}
+
+	public void saveCategory() {
+		AdminServiceImpl.getObject().addCategory(category);
+	}
 }
--- web/src/main/java/com/web/mb/AddProduct.java
+++ web/src/main/java/com/web/mb/AddProduct.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 
 import org.primefaces.event.FileUploadEvent;
@@ -18,58 +19,95 @@ import com.web.model.SupportURL;
 import com.web.service.AdminServiceImpl;
 
 @ManagedBean(name = "addProduct")
-@ViewScoped
+@SessionScoped
 public class AddProduct implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private Product pro;
 	private SupportURL selectedURL;
-	
+	private List<Category> proCategory;
+
 	public SupportURL getSelectedURL() {
 		return selectedURL;
 	}
+
 	public void setSelectedURL(SupportURL selectedURL) {
 		this.selectedURL = selectedURL;
 	}
-	private List<Category> proCategory;
+
 	public Product getPro() {
 		return pro;
 	}
+
 	public void setPro(Product pro) {
 		this.pro = pro;
 	}
+
 	public List<Category> getProCategory() {
 		return proCategory;
 	}
+
 	public void setProCategory(List<Category> proCategory) {
 		this.proCategory = proCategory;
 	}
-	
+
 	@PostConstruct
-	public void init(){
-		pro=new Product();
+	public void init() {
+		pro = new Product();
 		proCategory = new ArrayList<>();
 	}
-	public void saveProduct(){
-		if(null==pro.getDefaultImage())
+
+	public void saveProduct() {
+		System.out.println("Save...");
+		System.out.println("Save start"+pro);
+		if (null == pro.getDefaultImage())
 			pro.setDefaultImage(pro.getImgList().get(0));
+		System.out.println("Save cntd..");
 		AdminServiceImpl.getObject().saveProduct(pro);
+		System.out.println("Save completed");
 	}
-	public void delRecipe(){
+
+	public void addRecipe() {
+		System.out.println("add Recipe");
+		System.out.println(pro.getRecipe());
+		if (null == pro.getRecipe())
+			pro.setRecipe(new ArrayList<>());
+		System.out.println(pro.getRecipe().size());
+		SupportURL url = new SupportURL();
+		url.setVideo(false);
+		url.setDesc("a");
+		url.setUrl("a");
+		pro.getRecipe().add(url);
+		System.out.println(pro.getRecipe().size());
+	}
+
+	public void addBenifits() {
+		if (null == pro.getBenifits())
+			pro.setRecipe(new ArrayList<>());
+		pro.getBenifits().add(new SupportURL());
+	}
+
+	public void delRecipe() {
+		System.out.println(pro.getRecipe().size());
 		pro.getRecipe().remove(selectedURL);
+		selectedURL = null;
+		System.out.println(pro.getRecipe().size());
 	}
-	public void delBenifits(){
+
+	public void delBenifits() {
 		pro.getBenifits().remove(selectedURL);
+		selectedURL = null;
 	}
-	 public void handleFileUpload(FileUploadEvent event) {
-		 UploadedFile uploadedFile = event.getFile();
-		    String fileName = uploadedFile.getFileName();
-		    String contentType = uploadedFile.getContentType();
-		    byte[] contents = uploadedFile.getContents(); // Or getInputStream()
-		    ImageDetail id = new ImageDetail();
-		    id.setImgage(uploadedFile);
-		    id.setAltText(fileName);
-		    if(null == pro.getImgList())
-		    	pro.setImgList(new ArrayList<>());
-		    pro.getImgList().add(id);
-	 }
+
+	public void handleFileUpload(FileUploadEvent event) {
+		UploadedFile uploadedFile = event.getFile();
+		String fileName = uploadedFile.getFileName();
+		String contentType = uploadedFile.getContentType();
+		byte[] contents = uploadedFile.getContents(); // Or getInputStream()
+		ImageDetail id = new ImageDetail();
+		id.setImgage(uploadedFile);
+		id.setAltText(fileName);
+		if (null == pro.getImgList())
+			pro.setImgList(new ArrayList<>());
+		pro.getImgList().add(id);
+	}
 }
